e23f7eb2fa4de500bfd22c3a06251595d8326fcd
NikhilAlladi <91530305+Nikhilalladi5@users.noreply.github.com>
2024-05-28 14:35:38 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,10 +1,10 @@
-sg-id = "aws cloudformation list-exports --query "Exports[?Name=='Dev-MyInstanceSG'].Value" --output text"
-subnet-id = "aws cloudformation list-exports --query "Exports[?Name=='Dev-PublicSubnet01Id'].Value" --output text"
+sgId=$(aws cloudformation list-exports --query "Exports[?Name=='Dev-MyInstanceSG'].Value" --output text)
+subnetId=$(aws cloudformation list-exports --query "Exports[?Name=='Dev-PublicSubnet01Id'].Value" --output text)
 
 aws ec2 run-instances \
  --image-id ami-03afc392d26711a17 \
  --instance-type t2.micro \
  --key-name keypair \
- --security-group-ids ${sg-id} \
- --subnet-id ${subnet-id} \
+ --security-group-ids ${sgId} \
+ --subnet-id ${subnetId} \
  --associate-public-ip-address
